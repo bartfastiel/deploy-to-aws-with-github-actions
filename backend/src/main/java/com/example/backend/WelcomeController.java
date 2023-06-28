e5948533828d58b5ab73ce626754a105c388e3c9
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-06-28 11:55:08 +0200

    Add welcome message at path "/"

---
+++ backend/src/main/java/com/example/backend/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.example.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to the backend!";
+    }
+}
